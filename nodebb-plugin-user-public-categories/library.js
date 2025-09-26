6cca93031603e4756d0bfbe51bdd1b346af5c882
tbouri <tbouri@andrew.cmu.edu>
2025-09-26 13:28:21 +0000

    added helper functions getCount/bumpCount

--- nodebb-plugin-user-public-categories/library.js
+++ nodebb-plugin-user-public-categories/library.js
@@ -8,3 +8,12 @@ const User = require.main.require('./src/user');
 const PARENT_CID = 0;
 const MAX_PER_USER = 3;
 
+async function getCount(uid) {
+  const settings = (await User.getSettings(uid)) || {};
+  return Number(settings.userPublicCatCount || 0);
+}
+async function bumpCount(uid) {
+  const settings = (await User.getSettings(uid)) || {};
+  const next = Number(settings.userPublicCatCount || 0) + 1;
+  await User.updateSettings(uid, { ...settings, userPublicCatCount: next });
+}
