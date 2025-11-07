0f59905d251ca148375f9b97340cce1dede4b546
HairyCrabW <hangyiw@andrew.cmu.edu>
2025-11-07 21:10:03 +0000

    Connect to the actual translator api

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,17 +1,16 @@
 
 /* eslint-disable strict */
-//var request = require('request');
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
+translatorApi.translate = async function (postData) {
+	try {
+		const TRANSLATOR_API = 'http://crs-17313-sickfault-gpu:8080';
+		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+		const data = await response.json();
+		return [data.is_english, data.translated_content];
+	} catch (error) {
+		// Fallback to simple version if fetch fails
+		return ['is_english', postData];
+	}
 };
-
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
-// };
