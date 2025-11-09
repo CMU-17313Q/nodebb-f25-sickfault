c989e0147bdf325e21c0b16e1e5864f158f0ebc4
Harry Wen <hangyiw@andrew.cmu.edu>
2025-11-09 18:05:38 +0300

    Connect to the actual translator API deployed via Nginx

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,7 +5,7 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	try {
-		const TRANSLATOR_API = 'http://crs-17313-sickfault-gpu:8080';
+		const TRANSLATOR_API = 'http://crs-17313-sickfault-gpu.qatar.cmu.edu';
 		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
 		const data = await response.json();
 		return [data.is_english, data.translated_content];
